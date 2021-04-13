7848228eca802d0ff201414231b0b6fe8dd1b8eb
Eric Rabil <ericjrabil@gmail.com>
2021-04-12 21:04:22 -0400

    Memoize chat name to improve ChatSidebar rerender

--- src/hooks/useChatName.ts
+++ src/hooks/useChatName.ts
@@ -1,12 +1,20 @@
-import { ChatRepresentation } from "imcore-ajax-core";
+import { ChatRepresentation, ContactRepresentation } from "imcore-ajax-core";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { selectHandleIDToContact } from "../app/reducers/contacts";
 import { extractFormattedHandles } from "./useFormattedHandles";
 
+function computeChatName(chat: ChatRepresentation, handleIDToContact: Record<string, ContactRepresentation>): string {
+    return chat.displayName || extractFormattedHandles(chat.participants, handleIDToContact).join(", ") || chat.id;
+}
+
 export default function useChatName(chat?: ChatRepresentation | null): string | null {
     const handleIDToContact = useSelector(selectHandleIDToContact);
+    const [ chatName, setChatName ] = useState(() => chat ? computeChatName(chat, handleIDToContact) : null);
 
-    if (!chat) return null;
+    useEffect(() => {
+        setChatName(chat ? computeChatName(chat, handleIDToContact) : null);
+    }, [chat?.displayName, chat?.id, JSON.stringify(chat?.participants)]);
 
-    return chat.displayName || extractFormattedHandles(chat.participants, handleIDToContact).join(", ") || chat.id;
+    return chatName;
 }
